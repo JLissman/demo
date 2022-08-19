ae46ba62c25e69809c3b7ab757df83c4bcd404f5
Jonathan Lissman <78019749+JLissman@users.noreply.github.com>
2022-08-19 06:37:09 +0200

    init

--- venv/app.py
+++ venv/app.py
@@ -2,15 +2,16 @@
 import os
 import sys
 from flask import Flask, request, render_template
-import flask
 
+import mysql.connector
 
+#root paths and settings
 sys.path.insert(0, os.path.dirname(__file__))
-
 project_root = os.path.dirname(__file__)
 template_path = os.path.join(project_root, './templates')
 app = Flask(__name__, template_folder=template_path)
 app.static_folder = 'static'
+
 
 @app.route("/")
 def search():
@@ -22,6 +23,76 @@ def search():
         return render_template('index.html')
 
 
+@app.route("/search")
+def searchresults():
+    query = request.args.get('query')
+    print("Searching for "+query)
+    consultants = searchDB(query)
+    consultantsHTML = buildData(consultants)
+    #print("found "+str(consultants))
+    return render_template('search.html', keywords=query, data=consultantsHTML)
+
+@app.route('/testdb')
+def testdb():
+    try:
+        connection=get_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT version();")
+        db_version = cursor.fetchone()
+        print("You are connected to MySQL version: ", db_version)
+        close_connection(connection)
+        queryResult = getAllFromDb()
+        return '<h1>It works.</h1>'+'<h2>found '+str(queryResult)+'</h2>'
+    except Exception as e:
+        # see Terminal for description of the error
+        print("\nThe error:\n" + str(e) + "\n")
+        return '<h1>Something is broken.</h1>'
+
+def get_connection():
+    connection = mysql.connector.connect(host='localhost',
+                                         database='consultants',
+                                         user='root',
+                                         password='root')
+    return connection
+
+def close_connection(connection):
+    if connection:
+        connection.close()
+
+
+def getAllFromDb():
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM consult;")
+    allConsultants = cursor.fetchall()
+    close_connection(connection)
+    return allConsultants
+
+def searchDB(query):
+    connection = get_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM consult WHERE '"+query+"' IN (consultant_name, consultant_role, location);")
+    results = cursor.fetchall()
+    close_connection(connection)
+    return results
+
+def buildData(data):
+    query = request.args.get('query')
+    consultants = searchDB(query)
+    finalHTML = ""
+    #for debug
+    for consult in consultants:
+        start = '<li class="entity-result"> <div class="searchResult"> <div class="result-img"> <img class="profile-img"src="' + \
+                consult[4] + '"> </div> <div class="result-name">' + consult[1] + '</div> <div class="result-role">' + consult[2] + '</div> <div class="table"> <ul class="result-tags-list">'
+        tags = consult[3].split(",")
+        HTMLtags = ""
+        for tag in tags:
+            HTMLtags = HTMLtags + '<li class="tag">' + tag + '</li>'
+
+        end = '</ul></div><div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ligula urna,pharetra id accumsan sit amet, congue sed erat. Integer sed finibus enim. Donec sit amet bibendumsapien. </div> <div class="result-location"><img class="location-tag"src="https://flyclipart.com/thumb2/location-pin-emoji-934877.png">' + \
+              consult[5] + '</div></div></li>'
+        finalHTML = finalHTML + start + HTMLtags + end;
+    return finalHTML
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ venv/static/js/searchResults.js
@@ -0,0 +1,21 @@
+
+
+    function searchResults(data) {
+        console.log("running script")
+        data = {{data|tojson}}
+        console.log(data[0][2])
+        for (let x = 0; x <= data.length;x++) {
+            console.log("working on " + x + " result")
+            let start = '<li className="entity-result"> <div className="searchResult"> <div className="result-img"> <img className="profile-img"src="' + data[x][4] + '"> </div> <div className="result-name">' + data[x][1] + '</div> <div className="result-role">' + data[x][2] + '</div> <div className="table"> <ul className="result-tags-list">'
+            let tags = data[x][3].split(",")
+            HTMLtags = ""
+            for (let y = 0; y < tags.length; y++){
+                HTMLtags = HTMLtags + '<li className="tag">' + tags[y] + '</li>'
+                }
+            let end = '</ul></div><div className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ligula urna,pharetra id accumsan sit amet, congue sed erat. Integer sed finibus enim. Donec sit amet bibendumsapien. </div> <div className="result-location"><img className="location-tag"src="https://flyclipart.com/thumb2/location-pin-emoji-934877.png">'+data[x][5]+'</div></div></li>'
+
+        finishedHTML = start + HTMLtags + end;
+        console.log(finishedHTML)
+        }
+    document.getElementById("results").innerHTML = document.getElementById("results").innerHTML + finishedHTML;
+    }
